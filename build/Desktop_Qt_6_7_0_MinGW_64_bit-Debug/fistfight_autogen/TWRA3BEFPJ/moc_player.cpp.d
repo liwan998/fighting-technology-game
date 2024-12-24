e2735500af1e39f7b81ae46f8a3f83b2a15847e8
liwan998 <152075766+liwan998@users.noreply.github.com>
2024-12-24 21:29:46 +0800

    不要生成文件

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/fistfight_autogen/TWRA3BEFPJ/moc_player.cpp.d
+++
@@ -1,2 +0,0 @@
-E:/study/c++qt/game/fistfight/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/fistfight_autogen/TWRA3BEFPJ/moc_player.cpp: E:/study/c++qt/game/fistfight/character/player.h \
-  E:/study/c++qt/game/fistfight/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/fistfight_autogen/moc_predefs.h
--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/fistfight_autogen/TWRA3BEFPJ/moc_player_pirate.cpp.d
+++
@@ -1,3 +0,0 @@
-E:/study/c++qt/game/fistfight/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/fistfight_autogen/TWRA3BEFPJ/moc_player_pirate.cpp: E:/study/c++qt/game/fistfight/character/player_pirate.h \
-  E:/study/c++qt/game/fistfight/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/fistfight_autogen/moc_predefs.h \
-  E:/study/c++qt/game/fistfight/character/player.h
