81862492dc38d9c7122508bc66da165a00564e17
liwan998 <152075766+liwan998@users.noreply.github.com>
2024-12-30 22:44:18 +0800

    调整文件结构
    调整了文件结构使之更加美观

---
+++ role_state/queen_player_state_machine.cpp
@@ -0,0 +1,3 @@
+#include "queen_player_state_machine.h"
+
+queen_player_state_machine::queen_player_state_machine() {}
---
+++ role_state/queen_player_state_machine.h
@@ -0,0 +1,10 @@
+#ifndef QUEEN_PLAYER_STATE_MACHINE_H
+#define QUEEN_PLAYER_STATE_MACHINE_H
+
+class queen_player_state_machine
+{
+public:
+    queen_player_state_machine();
+};
+
+#endif // QUEEN_PLAYER_STATE_MACHINE_H
